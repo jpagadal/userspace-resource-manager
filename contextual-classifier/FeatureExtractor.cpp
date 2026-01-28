6a54990503ef7619f2cfed563799f385aaf16c84
Jagadeesh Pagadala <jpagadal@qti.qualcomm.com>
2026-01-28 14:59:17 +0530

    Fix compilation issues
    Signed-off-by: Jagadeesh Pagadala <jpagadal@qti.qualcomm.com>

--- contextual-classifier/FeatureExtractor.cpp
+++ contextual-classifier/FeatureExtractor.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
+#include "AuxRoutines.h"
 #include "FeatureExtractor.h"
 #include "FeaturePruner.h"
 #include "Logger.h"
